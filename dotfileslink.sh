9813bbeed50650831ad3e4fc394c3ff8eb19b1e5
Fukuda <fukuda1634924@gmail.com>
2017-08-31 22:59:38 +0900

    delete dotfile.sh

--- dotfileslink.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-ln -sf ~/dotfiles/.vim ~/.vim
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.zsh ~/.zsh
-ln -sf ~/dotfiles/.enhancd ~/.enhancd
